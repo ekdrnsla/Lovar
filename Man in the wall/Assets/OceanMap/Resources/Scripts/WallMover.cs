e3d9501310ed70adcc5b4d76ee3eed15e89500ad
025876 <y2k-112@daum.net>
2019-04-17 10:23:50 +0900

    no message

--- Man in the wall/Assets/OceanMap/Resources/Scripts/WallMover.cs
+++ Man in the wall/Assets/OceanMap/Resources/Scripts/WallMover.cs
@@ -6,7 +6,7 @@ public class WallMover : MonoBehaviour
 {
     public float speed = 1500;
     Vector3 oriPos;
-    bool CreateNewWallDelay = false;
+    bool isCreateWall = false;
     bool isMoving = false;
     public float ZPosition = 50;
     public float offset = 0.1f;
@@ -24,36 +24,37 @@ public class WallMover : MonoBehaviour
         createWallObj = FindObjectOfType<CreateWall>();
         animator = GetComponent<Animator>();
         playerObj = GameObject.Find("player");
+        transform.position = new Vector3(0, 0, ZPosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Jump") && !CreateNewWallDelay && !createWallObj._wallObject && playerObj.transform.position.y >= 0) StartCoroutine(CreateNewWall());
-        // if (Input.GetButton("Jump") && !CreateNewWallDelay && !createWallObj._wallObject && transform.position.z >= ZPosition && playerObj.transform.position.y >= 0) StartCoroutine(CreateNewWall());
-        if (transform.localScale.x <= offset) isMoving = true;
-
-        if (transform.position.z > -ZPosition / 4 && isMoving)
+        if (Input.GetButton("Jump") && !createWallObj._wallObject && !isCreateWall && transform.position.z == ZPosition && playerObj.transform.position.y >= 0)
+        {
+            isCreateWall = true;
+            StartCoroutine(CreateNewWall());
+            Debug.Log(1);
+        }
+        else if (transform.localScale.x <= offset) isMoving = true;
+        else if (isMoving)
         {
             gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -speed * Time.deltaTime);
             if (transform.position.z < -ZPosition / 4.5f)
             {
+                isMoving = false;
+                GetComponent<Rigidbody>().velocity = Vector3.zero;
                 animator.SetBool("isGo", false);
                 Destroy(createWallObj._wallObject);
+                transform.position = oriPos;
+                isCreateWall = false;
             }
-        }
-        else
-        {
-            transform.position = oriPos;
-            isMoving = false;
         }
     }
 
     IEnumerator CreateNewWall()
     {
-        CreateNewWallDelay = true;
         yield return new WaitUntil(() => createWallObj.Parse());
         animator.SetBool("isGo", true);
-        CreateNewWallDelay = false;
     }
 }
